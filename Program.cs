46449f279dec7b63dad8ff607e45b3742f34f078
HenryKasai <henrykasai1775@gmail.com>
2020-06-22 18:44:21 -0300

    Exercício Quase terminado.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MediaDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool terminar = false;
+            double resultado = 0;
+            while (!terminar)
+            {
+                int número;
+                Console.Write("Quantos números serão inseridos? ");
+                bool númerobool = Int32.TryParse(Console.ReadLine(), out número);
+                int i = 1;
+
+                if (!númerobool)
+                {
+                    Console.WriteLine("Valor inválido, pressione uma tecla para inserir outro valor.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    while (i <= número)
+                    {
+                        double n;
+                        Console.Write($"\nNúmero #{i}: ");
+                        bool nbool = Double.TryParse(Console.ReadLine(), out n);
+                        resultado = resultado + n;
+                        if (!nbool)
+                        {
+                            Console.WriteLine("Valor inválido, pressione uma tecla para inserir outro valor.");
+                        }
+                        else
+                        {
+                            i = i + 1;
+                        }
+                    }
+                    Console.WriteLine($"\nSoma...: {resultado}");
+                    Console.WriteLine($"Média...: {resultado / número}");
+                    Console.WriteLine($"Maior...: {máximo}");
+                    Console.WriteLine($"Menor...: {mínimo}");
+                    terminar = true;
+                }
+            }
+        }
+    }
+}
